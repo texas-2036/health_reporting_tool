f4d99f286381a4e3d24ed75a2a41b773e6dc5cad
vmmadathil <madathilvisakh@gmail.com>
2020-12-07 03:27:21 -0600

    maternity conditions complete

--- modules/maternity/maternity_cnd.R
+++ modules/maternity/maternity_cnd.R
@@ -15,13 +15,53 @@ maternity_cnd_ui <- function(id) {
              tabPanel(title="Diabetes", 
                       fluidRow(
                       column(width = 6,
-                             h2("Obesity"),
-                             includeMarkdown("markdown/aging/risk_factors/obesity.md")),
-                      column(width = 6))),
-             tabPanel("Heart Health", 
-                      h2("Obesity")),
-             tabPanel("Mental Health",  
-                      h2("obesity"))
+                             h2("Diabetes and Gestational Diabetes"),
+                             includeMarkdown("markdown/maternity/conditions/diabetes.md")),
+                      column(width = 6,
+                             h2("Diabetes Rates Among Women Ages 18-44"),
+                             )),
+                      fluidRow(
+                        column(width = 6,
+                               includeMarkdown("markdown/maternity/conditions/diabetes_graphic_callout.md")),
+                        column(width = 6,
+                               h2("Diabetes Rates Among Women Ages 18-44"),
+                               includeMarkdown("markdown/maternity/conditions/diabetes_top_right.md")
+                              )),
+                      
+                      ),
+             tabPanel(title = "Heart Health", 
+                      fluidRow(
+               column(width = 6,
+                      h2("High Blood Pressure"),
+                      includeMarkdown("markdown/maternity/conditions/high_blood_pressure.md")),
+               column(width = 6,
+                      includeMarkdown("markdown/maternity/conditions/high_blood_pressure_top_right.md")
+               )),
+               fluidRow(
+                 column(width = 6,
+                        h2("Preeclampsia"),
+                        includeMarkdown("markdown/maternity/conditions/preeclampsia.md")),
+                 column(width = 6
+                 ))
+               ), 
+             tabPanel(title = "Mental Health",  
+                      fluidRow(
+                        column(width = 6,
+                               h2("Depression During Pregnancy"),
+                               includeMarkdown("markdown/maternity/conditions/depression.md")),
+                        column(width = 6,
+                               h2("Treating Depression During Pregnancy"),
+                               includeMarkdown("markdown/maternity/conditions/treating_depression.md")
+                        )),
+                      fluidRow(
+                        column(width = 6,
+                               h2("Postpartum Depression"),
+                               includeMarkdown("markdown/maternity/conditions/postpartum_depression.md")),
+                        column(width = 6,
+                               includeMarkdown("markdown/maternity/conditions/postpartum_depression_bottom_right.md")
+                        ))
+                      
+                      )
              )
            )
   
