bf5e4584bf96b748eac6be525ce4f2e1106562f2
adortch <arielledortch@gmail.com>
2020-12-17 04:11:51 -0600

    Update intro

--- app.R
+++ app.R
@@ -285,6 +285,10 @@ body <- dashboardBody(
               tippy_this("explore_aging", 
               "As people age, health care needs increase due to chronic and acute conditions, 
                         but there are still many ways to improve quality of life and reduce health risks for Texas seniors."),
+            fluidRow(
+              column(width = 12,
+                      h2("Executive Summary"),
+                      includeMarkdown("markdown/intro/executive_summary.md"))),
             )),
     # **Maternity Section ---------------------------------------------------------------------------
     
